e14f25cdae25349bf5e149489edeb32b17dd4a40
Rafael Queiroz <r.queiroz@hotmail.com.br>
2020-07-28 19:30:20 -0300

    site base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './components/Menu'
+import BannerMain from './components/BannerMain'
+import Carousel from './components/Carousel'
+import dadosiniciais from './data/dados_iniciais.json'
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ background: "#141414" }}>
+      <Menu/>
+      <BannerMain videoTitle="O que faz uma desenvolvedora front-end" 
+        videoDescription="Um vídeo sobre frontend" 
+        url="https://www.youtube.com/watch?v=ZY3-MFxVdEw"/>
+
+      <Carousel
+        category={dadosiniciais.categorias[0]} 
+      />
+      <Carousel
+        category={dadosiniciais.categorias[1]} 
+      />
+      <Carousel
+        category={dadosiniciais.categorias[2]} 
+      />
+      <Carousel
+        category={dadosiniciais.categorias[3]} 
+      />
+      <Carousel
+        category={dadosiniciais.categorias[4]} 
+      />
+      <Carousel
+        category={dadosiniciais.categorias[5]} 
+      />
+      <Footer/>
     </div>
   );
 }
